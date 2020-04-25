72d18aad2d73ee2f82d90aa34a958bcf8e57c984
ZhangYanTJU <zytju@tju.edu.cn>
2020-04-25 20:01:40 +0200

    remove x y z

--- foam2Columns.C
+++ foam2Columns.C
@@ -121,27 +121,27 @@ int main(int argc, char *argv[])
         Info<<"outputFile======="<<outputFile<<endl;
         OFstream foam2Columns(outputFile);
 
-        foam2Columns
-            << "x" << "\t"
-            << "y" << "\t"
-            << "z" ;
+        //foam2Columns
+        //    << "x" << "\t"
+        //    << "y" << "\t"
+        //    << "z" ;
         forAll (selectedFieldNames, fieldI)
         {
-            foam2Columns << "\t" << selectedFieldNames[fieldI];
+            foam2Columns << selectedFieldNames[fieldI] << "\t";
         }
 
         foam2Columns << endl;
 
         forAll (allFields[0], cellI)
         {
-            foam2Columns
-                << mesh.C()[cellI].component(0) << "\t"
-                << mesh.C()[cellI].component(1) << "\t"
-                << mesh.C()[cellI].component(2);
+            //foam2Columns
+            //    << mesh.C()[cellI].component(0) << "\t"
+            //    << mesh.C()[cellI].component(1) << "\t"
+            //    << mesh.C()[cellI].component(2);
 
             for (auto Field : allFields)
             {
-                foam2Columns << "\t" << Field[cellI];
+                foam2Columns << Field[cellI] << "\t";
             }
             foam2Columns << endl;
         }
